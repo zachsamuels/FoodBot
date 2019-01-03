73262f8e389c22c268f21185cc6c11259e2e5399
CapnS <capnsurvivalist@gmail.com>
2019-01-03 13:44:51 -1000

    removed purge command

--- cogs/admin.py
+++ cogs/admin.py
@@ -7,16 +7,6 @@ class Admin:
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def purge(self, ctx, limit:int=100):
-        try:
-            await ctx.channel.purge(limit=limit+1)
-        except:
-            await ctx.send("Could not delete messages")
-        else:
-            await ctx.send("Deleted "+str(limit)+" messages.")
-
-    @commands.command()
-    @commands.has_permissions(manage_messages=True)
     async def cleanup(self, ctx, limit:int=25):
         def check(message):
             return message.content.startswith("food!") or message.author == self.bot.user
