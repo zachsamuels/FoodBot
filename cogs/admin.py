a45edcc7e386a7c0dd397677c75803c52755834c
CapnS <capnsurvivalist@gmail.com>
2019-02-20 16:08:14 -0600

    Adding mellow command

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,9 +1,36 @@
 import discord
 from discord.ext import commands
+import subprocess
+import aiohttp
 
 class Admin:
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    @commands.is_owner()
+    async def mellow(self, ctx, *, code):
+        with open("~/mellow/test.mlw", 'w') as f:
+            f.write(code)
+            f.close()
+        def runshell(code):
+            with subprocess.Popen(["/bin/bash", "-c", "python3 ~/mellow/mellow.py ~/mellow/test.mlw"], stdout=subprocess.PIPE, stderr=subprocess.PIPE) as process:
+                out, err = process.communicate(timeout=60)
+                if err:
+                    return [f"```fix\n{code}``` ```fix\n-- stdout --\n\n{out.decode()}``` ```fix\n-- stderr --\n\n{err.decode()}```", out.decode(), err.decode()]
+                else:
+                    return [f"```fix\n{code}``` ```fix\n-- stdout --\n\n{out.decode()}```", out.decode(), err.decode()]
+        result = await self.bot.loop.run_in_executor(None, runshell, code)
+        try:
+            await ctx.send(result[0])
+        except Exception:
+            await ctx.send(f"**:arrow_up: | Looks like output is too long. Attempting upload to Mystbin.**")
+            try:
+                async with aiohttp.ClientSession().post("http://mystb.in/documents", data=f"{result[1]}\n\n\n\n{result[2]}".encode('utf-8')) as post:
+                    post = await post.json()
+                    await ctx.send(f"**:white_check_mark: | http://mystb.in/{post['key']}**")
+            except Exception:
+                await ctx.send("**:x: | Couldn't upload to Mystbin.**") 
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
