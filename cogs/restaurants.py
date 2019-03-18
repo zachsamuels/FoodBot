8ebb09ce7a98fbbda92f7bba1060047789772282
CapnS <capnsurvivalist@gmail.com>
2019-03-18 18:11:37 -0500

    Adding Reviews to Restaurant Paginator

--- cogs/restaurants.py
+++ cogs/restaurants.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import random
+import asyncio
 
 class Restaurants(commands.Cog):
     def __init__(self, bot):
@@ -24,14 +25,53 @@ class Restaurants(commands.Cog):
         url = restaurant["url"]
         address = restaurant["location"]["address"]
         cuisine = restaurant["cuisines"]
-        cost = str(restaurant["average_cost_for_two"])
+        cost = restaurant["average_cost_for_two"]
         color = int(restaurant["user_rating"]["rating_color"], 16)
-        rating = str(restaurant["user_rating"]["aggregate_rating"])
+        rating = restaurant["user_rating"]["aggregate_rating"]
+        number = restaurant["phone_numbers"]
+        try:
+            reviews = random.sample(restaurant["all_reviews"], 3)
+        except:
+            reviews = restaurant["all_reviews"]
         em = discord.Embed(title=name, description="Cuisine: "+cuisine, url=url, color=discord.Color(color))
         em.add_field(name="Address", value=address)
         em.add_field(name="Avg. Cost per 2 People", value=cost)
         em.add_field(name="Rating", value=rating, inline=False)
-        await ctx.send(embed=em)
+        em.add_field(name="Phone Number", value=number)
+        revem = discord.Embed(title=name, description="Reviews", url=url, color=discord.Color(color))
+        for review in reviews:
+            user_name = review["user"]["name"]
+            rating = review["rating"]
+            rating_text = review["rating_text"]
+            review_text = review["review_text"]
+            timestamp = datetime.datetime(review["timestamp"]).strftime('%b %d, %Y')
+            revem.add_field(name=rating+ " - " + rating_text, value= review_text + "\n" + "    -" + user_name + " | " + timestamp)
+        embeds = {0:em, 1:revem}
+        x = 0
+        message = await ctx.send(embed=em)
+        def check(reaction,user):
+            return user == ctx.author and str(reaction.emoji) in emojis and reaction.message.id == message.id
+        emojis = ("\U000025c0","\U000025b6","\U000023f9")
+        for emoji in emojis:
+            await message.add_reaction(emoji)
+        while True:
+            try:
+                r, u = await self.bot.wait_for("reaction_add",check=check, timeout=60)
+            except asyncio.TimeoutError:
+                for emoji in emojis:
+                    await message.remove_reaction(emoji, ctx.me)
+                return
+            else:
+                try:
+                    await message.remove_reaction(r,u)
+                except:
+                    pass
+                if str(r.emoji) == "\U000023f9":
+                    return await message.delete()
+                else:
+                    x = int(not x)
+                em = embeds[x]
+                await message.edit(embed = em)
 
 
 def setup(bot):
